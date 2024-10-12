5ff4cb85cae72fa8124da96d13ff7c61da65b8ed
MozartofCode <bertan.berker@gmail.com>
2024-10-11 21:44:57 -0400

    Updated the GPT prompt function

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 from prompt import get_openai_response
 
 
+user_prompt = ""
+response = get_openai_response(user_prompt)
+print(response)
--- prompt.py
+++ prompt.py
@@ -1,31 +1,27 @@
 # Description: This script is used to call the OpenAI API to get a response for a given prompt. 
 # @Author: Bertan Berker
 
+
 from dotenv import load_dotenv
 import os
 import openai
 
-
 load_dotenv()
 openai.api_key = os.getenv('OPENAI_API_KEY')
-
-
 client = openai.OpenAI()
 
 
+# This function takes a prompt as input and returns the response from the OpenAI API
+# :prompt: str: The prompt to send to the OpenAI API
+# :return: str: The response from the OpenAI API
 def get_openai_response(prompt):
 
     completion = client.chat.completions.create(
         model="gpt-3.5-turbo-0125",
         messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "system", "content": "You are a world-class instagram influencer and you are excellent at generating posts"},
             {"role": "user", "content": prompt}
         ]
     )
 
     return completion.choices[0].message.content
-
-
-user_prompt = "Write a short story about a robot learning to love."
-response = get_openai_response(user_prompt)
-print(response)
